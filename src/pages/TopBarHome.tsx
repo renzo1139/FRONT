00e582f8f3764eac0ede69cf58f624dbe4d5e17c
alejandro13sa <156031781+alejandro13sa@users.noreply.github.com>
2024-01-14 13:34:47 -0500

    error
    topbarhome

--- src/pages/TopBarHome.tsx
+++ src/pages/TopBarHome.tsx
@@ -1,6 +1,3 @@
-// TopBarHome.tsx
-import React from "react";
-
 const TopBarHome = () => {
   return (
     <section className="bg-black text-white p-4 flex justify-center items-center" style={{ border: "2px solid #f0f0f0" }}>
